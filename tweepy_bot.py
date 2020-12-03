17d8f37fd3618217cadcdd1576da2709d7138ea6
Sunny <sunnyp2330@gmail.com>
2020-12-02 20:35:36 -0600

    Finished bot to tweet quotes from a website

--- tweepy_bot.py
+++ tweepy_bot.py
@@ -1,10 +1,13 @@
 from urllib.request import urlopen
+import tweepy
+
+# Web scrape from a quotes website to get a quote and the author
+
 url = "http://www.quotationspage.com/random.php"
 page = urlopen(url)
 
 html_bytes = page.read()
 html = html_bytes.decode(encoding='cp1252')
-print(html)
 start_index = html.find('<dt class=\"quote\"><a title=\"Click for further information about this quotation\" href=\"/quote/')
 quote_start = start_index + len('<dt class=\"quote\"><a title=\"Click for further information about this quotation\" href=\"/quote/')
 quote_end = html.find('</a> </dt>')
@@ -36,23 +39,26 @@ for a in raw_author:
 
 
 tweet = quote + "\n" + author
-print(tweet)
 
 
-#import tweepy
+# Open a text file to write the quote into, and later tweet the quote from
+with open("quotes.txt", "w") as file:
+    file.write(tweet)
 
 # Authenticate to Twitter
-# auth = tweepy.OAuthHandler("sMoA8AXezVoU0N8VZBp7kU9GQ",
-# "S3yQYThATrfnfgRucbQsK6qIq1SGFDDlabQE6ERG0cvlUBP6Fg")
-# auth.set_access_token("1327826996462841856-oujiUKbB4Dj3giY13EzUNlctLb6A6p",
-# "bWiyTozNLf95Dp2iJChGNoNJC30qCJUd7aYVBjkBlpcTw")
-#
-# api = tweepy.API(auth)
-#
-# try:
-#    api.verify_credentials()
-#    print("Authentication OK")
-# except:
-#    print("Error during authentication")
-#
-# api.update_status("Test tweet from Tweepy Python")
+
+auth = tweepy.OAuthHandler("sMoA8AXezVoU0N8VZBp7kU9GQ",
+"S3yQYThATrfnfgRucbQsK6qIq1SGFDDlabQE6ERG0cvlUBP6Fg")
+auth.set_access_token("1327826996462841856-oujiUKbB4Dj3giY13EzUNlctLb6A6p",
+"bWiyTozNLf95Dp2iJChGNoNJC30qCJUd7aYVBjkBlpcTw")
+
+api = tweepy.API(auth)
+
+
+# Try to tweet a quote, print if it succeeded or failed
+try:
+    with open("quotes.txt", "r") as file:
+        api.update_status(file.read())
+    print("Success")
+except:
+    print("Fail")
