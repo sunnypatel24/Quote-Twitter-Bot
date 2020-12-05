b594821654ddc2d1d68f3a41a94654582b8baf5a
Sunny <sunnyp2330@gmail.com>
2020-12-05 13:50:03 -0600

    Update tweepy_bot.py

--- tweepy_bot.py
+++ tweepy_bot.py
@@ -47,10 +47,10 @@ with open("quotes.txt", "w") as file:
 
 # Authenticate to Twitter
 
-auth = tweepy.OAuthHandler(consumer_key,
-consumer_secret)
-auth.set_access_token(token_key,
-token_secret)
+auth = tweepy.OAuthHandler("consumer_key",
+"consumer_secret")
+auth.set_access_token("token_key",
+"token_secret")
 
 api = tweepy.API(auth)
 
